0d2f166e976a03a526fb08f0714da806a22e9d71
Yoni19 <67630337+Yoni19@users.noreply.github.com>
2020-07-08 22:57:44 +0200

    Update 03_stairway.rb

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,25 +1,30 @@
 marche_actuelle = 0
+compte_essai = 0
 
 while marche_actuelle < 11
   resultat_du_roll = rand(1..6)
-  
+  #p resultat_du_roll
                 if marche_actuelle === 10
-                    puts "Tu es sur la marche 10, bravo ! Tu as a atteint la derniere marche, tu as gagné mon loulou ! heureux ?"
+                    puts "Tu es sur la marche 10, tu as a atteint la derniere marche, tu as gagné au bout de #{compte_essai} essais mon loulou ! heureux ?"
                     exit
                 elsif resultat_du_roll == 5 ||resultat_du_roll == 6 
-                    puts "Bravo ! tu as fait un #{resultat_du_roll}, tu montes de une place ! Tu es sur la marche #{marche_actuelle} !"
-                    marche_actuelle += 1
+                    puts "Bravo ! C'est l'essai n° #{compte_essai}, tu as fait un #{resultat_du_roll}, tu montes de une place ! Tu es sur la marche #{ marche_actuelle} essaie encore ! "
+                    compte_essai +=1
+                   marche_actuelle += 1
          
                 elsif resultat_du_roll == 1 
-                    puts "Aie !tu as fait un #{resultat_du_roll}, tu descend d'une place, tu es sur la marche #{ marche_actuelle} !"
-                    marche_actuelle -= 1 
+          puts "Aie !  C'est l'essai n° #{compte_essai}, tu as fait un #{resultat_du_roll}, tu descend d'une place, tu es sur la marche #{ marche_actuelle} essaie encore !  "
+                    compte_essai +=1
+                marche_actuelle -= 1 
 
                 elsif resultat_du_roll == 2 || resultat_du_roll == 3 ||resultat_du_roll == 4 
-                    puts "Pas de chance, tu as fait un #{resultat_du_roll}, tu ne bouges pas, tu es sur la marche  #{marche_actuelle} !"
-                if marche_actuelle < 0
-                       marche_actuelle = 0
-                    
+                   puts "Pas de chance, c'est l'essai n° #{compte_essai}, tu as fait un #{resultat_du_roll}, tu ne bouges pas, tu es sur la marche  #{ marche_actuelle} essaie encore ! "
+                    compte_essai +=1
+                    if marche_actuelle <0
+                        marche_actuelle = 0
+                        compte_essai +=1
+                    compte-essai +=1
                     end
 
-                end
+                  end
 end
