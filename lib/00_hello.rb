11ac6357a5937a2a39b37e166952fd2a97a91971
Yoni19 <anthony.krief@hotmail.fr>
2020-07-08 15:57:25 +0200

    Ajout exo Anthony

---
+++ lib/00_hello.rb
@@ -0,0 +1,18 @@
+#Maintenant rajoute une méthode ask_first_name qui demande à l'utilisateur son prénom et retourne le résultat avec un return. 
+#Combine-la avec ta say_hello(first_name) pour avoir un programme qui demande à l'utilisateur son prénom et lui dit bonjour. Quelque chose comme :
+
+
+
+def ask_first_name
+    puts " quel est votre prenom ?"
+    return <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<first_name = gets.chomp
+
+end
+
+
+def say_hello(first_name)
+    puts "Bonjour, #{first_name}"
+end
+
+first_name = ask_first_name
+say_hello(first_name)
